43f11d61d7c59e0b6cc2687eefd5e3f37322ae6c
roiblum1 <roiblum05@gmail.com>
2024-10-31 09:54:07 +0200

    created requirments.txt and retrive the tables

--- bpf.py
+++ bpf.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import os 
-import subprocess
 from typing import Optional
 from pydantic import BaseModel
+from bcc import BPF
 
 class PacketInformation(BaseModel):
     src_ip: str
@@ -13,7 +13,7 @@ class PacketInformation(BaseModel):
     data_length: Optional[int] = 0 
     data: str
     
-def read_file():
+def read_file_tracing():
     print("Started capturing.\nPress ctrl+c to stop...")
     packet_list = []
     try:
@@ -35,13 +35,18 @@ def read_file():
     except KeyboardInterrupt:
         print("\nStoppped Capturing")
         return packet_list
-
 '''
 packet exmaple 
 src_ip:0.0.0.0,dest_ip:0.0.0.0,tot_len:108,ttl:64,protocol:TCP,data:000000004c0f8588
 '''
+
+def read_maps():
+    BPF.get_table("packet_map")
+    BPF.get_table("aggregate_map")
+    BPF.get_table("global_map") 
+    
 def main():
-    packet_list = read_file()
+    packet_list = read_file_tracing()
     print(packet_list)
     
 if __name__ == "__main__":
